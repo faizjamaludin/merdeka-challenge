837f1b2b614aa9861474560f115e7032bb3678cf
Faiz Jamaludin <faizjamaludin@Faizs-MacBook-Pro.local>
2021-08-08 02:13:06 +0800

    init file

---
+++ resources/views/layout/layout.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    
+    <link rel="stylesheet" href="{{ asset('css/style.css')}}">
+    
+    <title>Cyberjaya.</title>
+</head>
+<body>
+    <section>
+        <header>
+           <a href="" class="logo">Cyberjaya.</a>
+           <ul class="nav">
+               <li><a href="#" class="active">Blog</a></li>
+               <li><a href="#">About</a></li>
+               <li><a href="#">Contact</a></li>
+           </ul>
+        </header>
+        <div class="content">
+            <div class="imgbox">
+                <img class="img-main" src="{{asset('img/tunku-abdul-rahman.png')}}" alt="">
+            </div>
+            <div class="textbox">
+                <div class="legend">
+                    <p class="date">August 6 2021</p>
+                </div>
+                <h2><a href="#">Merdeka</a></h2>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                    when an unknown printer took a galley of type and scrambled it to make a type 
+                    specimen book. It has survived not only five centuries, but also the leap into 
+                    electronic typesetting, remaining essentially unchanged.
+                </p>
+                <a href="#">More</a>
+
+                <div class="author">
+                    <img class="author-img" src="" alt="">
+                    <div class="authortext"></div>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
